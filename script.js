8524cb1514a5aa04f9c076279f6e660529dab94b
Plasnid <stuart@sackler.ca>
2022-02-22 12:11:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+/**
+ * * Midterm 2601 - Winter 2022 - 60 Marks Total
+ * 
+ * 
+ * * 1.  Link the script.js file to the html in the head tag - 5 marks
+ */
+
+/**
+ * * 2. Declaring variables - 10 marks
+ * 
+ * * name and set the following variables
+ * 
+ * * a string containing the name of a food from your favourite restaurant
+ * * a number containing the speed limit of a street
+ * * an boolean about a topic of your choice
+ * * an array listing items used in a hobby you partake in
+ * * an object describing your favourite food
+ */
+
+/**
+ * * 3. Function declarations and string literals - 20 marks
+ * * 
+ * * Create a function declaration that takes in two parameters
+ * *
+ * * Use the two parameters to fill in the following sentence using a template literal
+ * 
+ * * "My favourite ______ is called _____!"
+ * 
+ * * Have the function declaration return the sentence and write it to a P tag that you make with JavaScript
+ * * Put the created p tag containing the sentence into the body of your HTML using DOM Manipulation
+ */
+
+/**
+ * * 4. Arrays and Looping - 15 Marks
+ * 
+ * * Create an array about 1 of the following topics:
+ * * -foods
+ * * -world cities
+ * * -animals
+ * * -tv shows
+ * 
+ * * Be sure to have at least 5 entries in your array.  Each entry must be a string.
+ * * Create a for loop and add each entry to the screen(in a p tag) and the console log. 
+ */
+
+/**
+ * * 5. Decisions - 10 Marks
+ * 
+ * * Create a function declaration containing an if statement about what to eat.  
+ * 
+ * * Using an if/else if/else, create separate blocks for answers for each of the following
+ * * - carnavores(meat eaters)
+ * * - herbivores(plant eaters)
+ * * - pescavores(fish eaters)
+ * * - omnivores(eats anything)
+ * 
+ * * Have the function return a phrase.  Console log the returned phrase
+ */
